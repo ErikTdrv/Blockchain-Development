789b8e57fd506665fbe3ff87719206a1447f03f9
Erik Todorov <eriktdrv@gmail.com>
2023-05-01 13:43:25 +0300

    Adding a payment

--- Home Repair Service/Contract.sol
+++ Home Repair Service/Contract.sol
@@ -9,22 +9,39 @@ pragma solidity >=0.8.2 <0.9.0;
  */
 contract HomeRepair {
     address public owner;
-    mapping (uint256 => string) public requests;
-    mapping (uint256 => bool) public acceptedRequests;
+    mapping(uint256 => string) public requests;
+    mapping(uint256 => bool) public acceptedRequests;
+    mapping (uint256 => uint256) public prices;
 
     constructor() {
         owner = msg.sender;
     }
 
     // 1. Add a repair request
-    function addRepairRequest(string memory requestDescription, uint256 requestId) external {
+    function addRepairRequest(
+        string memory requestDescription,
+        uint256 requestId
+    ) external {
         requests[requestId] = requestDescription;
         acceptedRequests[requestId] = false;
     }
+
     // 2. Accept a repair request
     function acceptRepairRequest(uint256 requestId) public {
         acceptedRequests[requestId] = true;
     }
 
-
+    // 3. Add a payment
+    function addPayment(uint256 id, uint256 price) public {
+        require(
+            bytes(requests[id]).length > 0,
+            "Request with given id does not exist!"
+        );
+        require(price > 0, "Price should be a valid number!");
+        require(
+            acceptedRequests[id] == true,
+            "Request has not been accepted yet!"
+        );
+        prices[id] = price;
+    }
 }
